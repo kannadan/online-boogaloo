e9e219857656181e6e9c962492743a4ce4ca3b83
JanneEskola <jaeskola@student.oulu.fi>
2020-11-27 16:43:31 +0200

    main.py
    How do I command line git

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
-from ruuvitag_sensor.ruuvi import RuuviTagSensor
-import ruuvitag_sensor.log
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
 import logging
-from random import randint
+import os
+import ruuvitag_sensor.log
+from credentials import credentials
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
+from telegram.ext import Updater, CommandHandler
 
-api = 
 
 def get_data():
     ruuvitag_sensor.log.enable_console()
     data = RuuviTagSensor.find_ruuvitags()
     return data
-    
+
+
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
 def weather(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=get_data())
-    
 
-        
+
+def otit(update, context):
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('otit_logo_fill.jpg', 'rb'))
+
+
 if __name__ == '__main__':
-    updater = Updater(token=api, use_context=True)
-
+    creds = credentials.require(['api'])
+    updater = Updater(token=creds.api)
 
     dispatcher = updater.dispatcher
 
-
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
-    
+
     weather_handler = CommandHandler('weather', weather)
     dispatcher.add_handler(weather_handler)
-    
 
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                         level=logging.INFO)
-                         
-    updater.start_polling()                     
-                     
+    otit_handler = CommandHandler('otit', otit)
+    dispatcher.add_handler(otit_handler)
 
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+    updater.start_polling()
