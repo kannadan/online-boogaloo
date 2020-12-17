e624bc33c52c3aef5d06b47b22345fd73d61b133
agilityriina <riina_annunen@hotmail.com>
2020-12-18 00:14:11 +0200

    Added the feature to get data from two ruuvitags. Not quite working yet though

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -16,9 +16,10 @@
 int scanTime = 5; //In seconds
 BLEScan* pBLEScan;
 int temp, hum, pressure, ax, ay, az, voltage_power, voltage, power, rssi_ruuvi, movement, measurement;
+int temp2, hum2, pressure2, ax2, ay2, az2, voltage_power2, voltage2, power2, rssi_ruuvi2, movement2, measurement2;
 
-const char* ssid     = "OTiT";
-const char* password = "oh8taoh8ta";
+const char* ssid     = "TekinJäsenyysHäikäisee";
+const char* password = "JukkaOrava!123";
 
 const char* ntpServer = "pool.ntp.org";
 
@@ -29,16 +30,19 @@ String path = "/ruuvidata";
 String nodeID = "Node1";
  
 String ruuvimac = "EB:3F:74:AB:F0:D7";
-
+String ruuvimac2 = "CC:F2:A1:AE:04:4D";
 
 const long  gmtOffset_sec = 7200;
 const int   daylightOffset_sec = 3600;
-
-
-//Converts hexadecimal values to decimal values
 int hexadecimalToDecimal(String hexVal)
 {
     int len = hexVal.length();
+
+    for (int i=0; i < len; ++i)
+    {
+      byte val = (byte)hexVal[i];
+      Serial.println(val);
+    }
     int base = 1;
 
     int dec_val = 0;
@@ -62,16 +66,25 @@ int hexadecimalToDecimal(String hexVal)
 }
 
 //Decodes RUUVI raw data and arranges it in an array
-void decodeRuuvi(String hex_data, int rssi){
+void decodeRuuvi(String hex_data, int rssi, int id){
     if(hex_data.substring(4, 6) == "05"){
-        temp = hexadecimalToDecimal(hex_data.substring(6, 10))*0.005;
-        hum = hexadecimalToDecimal(hex_data.substring(10, 14))*0.0025;
-        pressure = hexadecimalToDecimal(hex_data.substring(14, 18))*1+50000;
-       
-        ax = hexadecimalToDecimal(hex_data.substring(18, 22));
-        ay = hexadecimalToDecimal(hex_data.substring(22, 26));
-        az = hexadecimalToDecimal(hex_data.substring(26, 30));     
-
+        //Serial.print(F(hex_data));
+        if (id == 1){
+          temp = hexadecimalToDecimal(hex_data.substring(6, 10))*0.005;
+          hum = hexadecimalToDecimal(hex_data.substring(10, 14))*0.0025;
+          pressure = hexadecimalToDecimal(hex_data.substring(14, 18))*1+50000;
+          ax = hexadecimalToDecimal(hex_data.substring(18, 22));
+          ay = hexadecimalToDecimal(hex_data.substring(22, 26));
+          az = hexadecimalToDecimal(hex_data.substring(26, 30)); 
+        }
+        else{
+          temp2 = hexadecimalToDecimal(hex_data.substring(6, 10))*0.005;
+          hum2 = hexadecimalToDecimal(hex_data.substring(10, 14))*0.0025;
+          pressure2 = hexadecimalToDecimal(hex_data.substring(14, 18))*1+50000;
+          ax2 = hexadecimalToDecimal(hex_data.substring(18, 22));
+          ay2 = hexadecimalToDecimal(hex_data.substring(22, 26));
+          az2 = hexadecimalToDecimal(hex_data.substring(26, 30));     
+        }
         if(ax > 0xF000){
           ax = ax - (1 << 16);
         }
@@ -81,16 +94,17 @@ void decodeRuuvi(String hex_data, int rssi){
         if (az > 0xF000){
           az = az - (1 << 16);
         }
-
+      
         voltage_power = hexadecimalToDecimal(hex_data.substring(30, 34));
         voltage = (int)((voltage_power & 0x0b1111111111100000) >> 5) + 1600;
         power = (int)(voltage_power & 0x0b0000000000011111) - 40;
 
         rssi_ruuvi = rssi;
-
+        
         movement = hexadecimalToDecimal(hex_data.substring(34, 36));
         measurement = hexadecimalToDecimal(hex_data.substring(36, 40));
     }
+    
 }
 
 void sendData(){
@@ -111,6 +125,19 @@ void sendData(){
     Serial.println("error sending");
     Serial.println(firebaseData2.errorReason());
   }
+  FirebaseJson json2;
+  json2.add("air-pressure", pressure2);
+  json2.add("humidity", hum2);
+  json2.add("id", "ruuvi-2");
+  json2.add("temperature", temp2);
+  json2.set("timestamp", (double) getTimeStamp());
+  String node2 = uuid4();
+  if (Firebase.setJSON(firebaseData2, "/" + node2, json2)) {
+    Serial.println(firebaseData2.dataType());
+  } else {
+    Serial.println("error sending");
+    Serial.println(firebaseData2.errorReason());
+  }
 }
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
@@ -119,8 +146,17 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
       if(ruuvimac.indexOf(advertisedDevice.getAddress().toString().c_str()) >= 0){ //If the scanned MAC address is in the identified MAC address String
         String raw_data = String(BLEUtils::buildHexData(nullptr, (uint8_t*)advertisedDevice.getManufacturerData().data(), advertisedDevice.getManufacturerData().length()));
         raw_data.toUpperCase();
-        decodeRuuvi(raw_data, advertisedDevice.getRSSI());     
-        //sendData();  
+        int ruuvi_id = 1;
+        decodeRuuvi(raw_data, advertisedDevice.getRSSI(),ruuvi_id);
+        
+        //sendData();
+      }  
+      else if(ruuvimac2.indexOf(advertisedDevice.getAddress().toString().c_str()) >= 0){ //If the scanned MAC address is in the identified MAC address String
+        String raw_data = String(BLEUtils::buildHexData(nullptr, (uint8_t*)advertisedDevice.getManufacturerData().data(), advertisedDevice.getManufacturerData().length()));
+        raw_data.toUpperCase();
+        int ruuvi_id = 2;
+        decodeRuuvi(raw_data, advertisedDevice.getRSSI(), ruuvi_id);
+        //sendData();
       }  
     }
 };
@@ -128,7 +164,7 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
 long getTimeStamp(){
   struct tm timeinfo;
   if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
+    //Serial.println("Failed to obtain time");
     return 0;
   }
   //Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
@@ -172,7 +208,7 @@ int value = false;
 char incoming;
 void loop()
 {
-
+    Serial.print("OLLAAN LOOPISSA");
     BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
     pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
     sendData();
@@ -183,20 +219,20 @@ String uuid4(){
   String res = "";
   res = res + hex_print(8,0,15);
   res = res + "-";
-  Serial.print("-");
+  //Serial.print("-");
   res = res + hex_print(4,0,15);
   res = res + "-4";
-  Serial.print("-");
-  Serial.print("4");
+  //Serial.print("-");
+  //Serial.print("4");
   res = res + hex_print(3,0,15);
   res = res + "-";
-  Serial.print("-");
+  //Serial.print("-");
   res = res + hex_print(1,8,11);
   res = res + hex_print(3,0,15);
   res = res + "-";
-  Serial.print("-");
+  //Serial.print("-");
   res = res + hex_print(12,0,15);
-  Serial.println("");
+  //Serial.println("");
   return res;
 }
 
@@ -205,7 +241,7 @@ String hex_print(int n,int mn ,int mx ){
   for(;n>0;n--){
     int X = random(mn,mx);
     res = res + String(X, HEX);
-    Serial.print ( X ,HEX );
+    //Serial.print ( X ,HEX );
   }
   return res;
 }
