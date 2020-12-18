63a5b089ae78f4d064a4f0c614b0d4c1941b2ab5
agilityriina <riina_annunen@hotmail.com>
2020-12-18 02:03:35 +0200

    Made it available for user to select timescale of the data and from which device wants it from

--- firebase.py
+++ firebase.py
@@ -8,7 +8,7 @@ class DataBase:
 
     def __init__(self):
         # Use the application default credentials
-        self.cred = credentials.Certificate('online-boogaloo-firebase-adminsdk-7qkz5-1d8b512e88.json')
+        self.cred = credentials.Certificate('online-boogaloo-firebase-adminsdk-7qkz5-8db06a938a.json')
         firebase_admin.initialize_app(self.cred, {
             'databaseURL': 'https://online-boogaloo.firebaseio.com/',
         })
@@ -16,18 +16,45 @@ class DataBase:
         self.ref = db.reference()
         self.data = self.ref.get()
 
-    def get_latest(self):
-        self.data = self.ref.get()
-        lat_stamp = 0
+    #def get_latest(self):
+        #self.data = self.ref.get()
+        #at_stamp = 0
+        #for point in self.data:
+            #data_point = self.data[point]
+            #if lat_stamp < data_point['timestamp']:
+                #lat_stamp = data_point['timestamp']
+                #latest = data_point
+
+    #date = None
+    #ruuvi_id = None
+    def get_latest(self, latest):
+        air_pressure = []
+        humidity = []
+        temperature = []
+        times = []
+        #Latest is what the user has selected in Telegram bot. We also need 2 times the datapoints because we have two ruuvitags. Data is delivered into database every 10 minutes
+        if latest == "latest from fridge" or latest == "latest from inside":
+            datapoints = 2 
+        elif latest == "hour from fridge" or latest == "hour from inside":
+            datapoints = 12
+        self.data = self.ref.order_by_child('timestamp').limit_to_last(datapoints).get()
         for point in self.data:
             data_point = self.data[point]
-            if lat_stamp < data_point['timestamp']:
-                lat_stamp = data_point['timestamp']
-                latest = data_point
-
-        air_pressure = latest['air-pressure']
-        humidity = latest['humidity']
-        ruuvi_id = latest['id']
-        temperature = latest['temperature']
-        date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(latest['timestamp']))
-        return air_pressure, humidity, ruuvi_id, temperature, date
+            ruuvi_id = data_point["id"]
+            #We only want the data what user wants
+            con1 = "fridge" in latest and ruuvi_id == "ruuvi-1"
+            con2 = "inside" in latest and ruuvi_id == "ruuvi-2"
+            if con1 or con2:
+                air_pressure.append(data_point["air-pressure"])
+                humidity.append(data_point["humidity"])
+                temperature.append(data_point["temperature"])
+                stamp = data_point['timestamp']
+                times.append(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(stamp)))
+            
+        #air_pressure = latest['air-pressure']
+        #humidity = latest['humidity']
+        #ruuvi_id = latest['id']
+        #temperature = latest['temperature']
+        #date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(latest['timestamp']))
+        #return air_pressure, humidity, ruuvi_id, temperature, date
+        return air_pressure, humidity, temperature, times
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import logging
 import os
 import ruuvitag_sensor.log
-from credentials import credentials
+#from credentials import credentials
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
 from firebase import DataBase
+from telegram import ReplyKeyboardMarkup
 
+data = {'timescale':""}
+TIMES = 1
 
 def get_data():
     ruuvitag_sensor.log.enable_console()
@@ -14,7 +17,7 @@ def get_data():
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a weather bot. Hi!")
 
 
 def weather(update, context):
@@ -25,7 +28,6 @@ def weather(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text='From {rid}, Time: {date}, Temperature: {temp}°C,'
                                                                     'Humidity: {hum}, Air pressure: {ap}'
                              .format(rid=rid, date=date, temp=temp, hum=hum, ap=ap))
-
 
 def air_pressure(update, context):
     ap, hum, rid, temp, date = db.get_latest()
@@ -38,24 +40,45 @@ def humidity(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text='Humidity from {rid} at {date}: {hum}'
                              .format(rid=rid, date=date, hum=hum))
 
-
 def temperature(update, context):
-    ap, hum, rid, temp, date = db.get_latest()
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text='Temperature from {rid} at {date}: {temp}°C'.format(rid=rid, date=date, temp=temp))
-
+    global data
+    data = {"timescale": ""}
+    reply = [["latest from fridge", "hour from fridge", "latest from inside", "hour from inside"]] #week and year are too long 
+    update.message.reply_text("Hi! From what timeline and from where would you like to see the temperature?", reply_markup=ReplyKeyboardMarkup(reply, one_time_keyboard=False),)
+    return TIMES
+    
+def temperature2(update, context):
+    data["timescale"] = update.message.text
+     
+    #ap, hum, rid, temp, date = db.get_latest()
+    ap, hum, temp, time = db.get_latest( data["timescale"])
+    textt = ""
+    i=0
+    #Make a list of the data
+    for a in ap:
+        textt += 'Temperature from {time}: {temp}°C'.format(time=time[i], temp=temp[i])
+        textt += "\n"
+        i = i+1
+    context.bot.send_message(chat_id=update.effective_chat.id, text=textt)
 
 def weather_graph(update, context):
-    ap, hum, rid, temp, date = db.get_latest()
+    #ap, hum, rid, temp, date = db.get_latest()
+    ap, hum, temp, time = db.get_latest()
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('graph.png', 'rb'))
     context.bot.send_message(chat_id=update.effective_chat.id, text='Weather graph from last 7 days')
 
+def cancel(update, context):
+
+    update.message.reply_text('canceled')
+
+    # end of conversation
+    return ConversationHandler.END
 
 if __name__ == '__main__':
     db = DataBase()
-    creds = credentials.require(['api'])
-    updater = Updater(token=creds.api)
-
+    #creds = credentials.require(['api'])
+    #pdater = Updater(token=creds.api)
+    updater = Updater(token="1459062128:AAHekgpf4yzZQ3qyxTBLrTUo5FHo9e0UYZk")
     dispatcher = updater.dispatcher
 
     start_handler = CommandHandler('start', start)  # Start
@@ -66,11 +89,27 @@ if __name__ == '__main__':
     dispatcher.add_handler(pressure_handler)
     hum_handler = CommandHandler('humidity', humidity)  # Humidity
     dispatcher.add_handler(hum_handler)
-    temp_handler = CommandHandler('temperature', temperature)  # Humidity
-    dispatcher.add_handler(temp_handler)
+    #temp_handler = CommandHandler('temperature', temperature)  # 
+    #dispatcher.add_handler(temp_handler)
     graph_handler = CommandHandler('weather_graph', weather_graph)  # Weather graph
     dispatcher.add_handler(graph_handler)
 
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+    #This allows the user to "communicate" with the bot. Allows the choosing of data point
+    #The temperature2 in MessageHandler means where do we go after temperature function has finished and returned TIMES
+    conversation_handler = ConversationHandlerd
+        entry_points=[CommandHandler('temperature', temperature)],
+        states={
+            TIMES: [
+                MessageHandler(Filters.text, temperature2)
+            ],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+        )                
+
+    dispatcher.add_handler(conversation_handler)
+    
+    
+    
     updater.start_polling()
